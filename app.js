a17be87803a9da1d75e382148e28420d5c5c7ee4
Rand92 <>
2021-06-30 14:53:44 +0000

    Creaded app.Js

---
+++ app.js
@@ -0,0 +1,20 @@
+var FirstName = "Rand Albqour"
+Age = 29
+console.log ("My Name is " + FirstName + " And i'm " + Age + " year-old")
+X = 20
+Y = 21
+var sum = X+Y
+console.log(sum)
+D = 50
+F = 75
+if (D =>45){ console.log ("Have a nice day")}
+else { console.log("Go Away")}
+
+// a random comment
+
+var massage = prompt("Write a nice Massage to yourself")
+console.log (massage)
+alert("You deserve it :) ")
+
+if (massage != null){document.write("<h4>" + massage + "</h4>")}
+
